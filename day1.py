276781fdde72e3aeffc25c57149a41f0d1b65023
vaibhavi-madke <vaibhavi.madke@gmail.com>
2026-05-09 18:20:51 +0530

    Day 1 - My AI/ML Journey Begins

---
+++ day1.py
@@ -0,0 +1,24 @@
+# DAY 1:- My Aiml Journey
+# Name:- Vaibhavi Madke
+# Date:- 8 May 2026
+print("My Name is Vaibhavi")
+print("I am 19 years old")
+print("I am studying in BSc Aiml")
+print("Today is Day 1 of My Journey")
+print("I will become an Ai Engineer")
+# Variables-Storing Information
+my_name="Vaibhavi Madke"
+my_age=19
+my_goal="Ai Engineer"
+cgpa_target=9.76
+print("\n---About Me---")
+print("My Name: ",my_name)
+print("My Age: ",my_age)
+print("My Goal: ",my_goal)
+print("My CGPA Target: ",cgpa_target)
+#Simple Calculation
+days_in_year= 365
+hours_per_day= 2
+total_hours= days_in_year*hours_per_day
+print("\n If I Study",hours_per_day,"Hours Daily,")
+print("I Will Have Studied ",total_hours,"Hours By End Of The Year!")
